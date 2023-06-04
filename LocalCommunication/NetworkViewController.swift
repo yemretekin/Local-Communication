159e46cc5de5d895d9357eeb48bb82a0ae5e6730
yemretekin <emretekin08@gmail.com>
2023-06-04 23:09:39 +0300

    first commit

---
+++ LocalCommunication/NetworkViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkViewController.swift
+//  LocalCommunication
+//
+//  Created by Emre Tekin on 2.06.2023.
+//
+
+import UIKit
+
+class NetworkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
